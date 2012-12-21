dfb62012b9b489847768aa9cb3dd937bdfabdc0f
Todd Poynor <toddpoynor@google.com>
2012-12-21 15:13:01 -0800

    cpufreq: interactive: default go_hispeed_load 99%, doc updates
    Update default go_hispeed_load from 85% to 99%.  Recent changes to the
    governor now use a default target_load of 90%.  go_hispeed_load should
    not be lower than the target load for hispeed_freq, which could lead
    to oscillating speed decisions.  Other recent changes reduce the need
    to dampen speed jumps on load spikes, while input event boosts from
    userspace are the preferred method for anticipating load spikes with
    UI impacts.
    
    General update to the documentation to reflect recent changes.
    
    Change-Id: I1b92f3091f42c04b10503cd1169a943b5dfd6faf
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_interactive.c

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -69,14 +69,7 @@ static unsigned int hispeed_freq;
 
 /* Go to hi speed when CPU load at or above this value. */
 #define DEFAULT_GO_HISPEED_LOAD 99
-static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
-
-/* Target load.  Lower values result in higher CPU speeds. */
-#define DEFAULT_TARGET_LOAD 90
-static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
-static spinlock_t target_loads_lock;
-static unsigned int *target_loads = default_target_loads;
-static int ntarget_loads = ARRAY_SIZE(default_target_loads);
+static unsigned long go_hispeed_load;
 
 /* Target load.  Lower values result in higher CPU speeds. */
 #define DEFAULT_TARGET_LOAD 90
