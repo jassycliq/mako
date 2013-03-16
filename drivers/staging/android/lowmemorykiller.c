4c08a5a67821ba1058ba96789c22b16869fb4b47
Dmitry Voytik <dvv.kernel@gmail.com>
2013-03-16 11:37:26 +0400

    staging: android: lowmemorykiller: well-marked debug print
    Add "lowmemorykiller:" prefix to the debug print so it's easier to
    analyse LMK's debug output:
    dmesg | grep lowmemorykiller
    
    Signed-off-by: Dmitry Voytik <dvv.kernel@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -30,6 +30,8 @@
  *
  */
 
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
@@ -65,7 +67,7 @@ extern int compact_nodes(bool sync);
 #define lowmem_print(level, x...)			\
 	do {						\
 		if (lowmem_debug_level >= (level))	\
-			printk(x);			\
+			pr_info(x);			\
 	} while (0)
 
 static int test_task_flag(struct task_struct *p, int flag)
