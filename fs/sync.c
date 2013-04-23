7cb9860b8bfeed3cfde8904c48e4d063ab67cb95
Jose Salgado <josalgado12709@gmail.com>
2013-04-23 12:29:19 -0700

    Revert "switch do_fsync() to fget_light()"
    This reverts commit b1f96fd7d00f381da2207ade47036950d99f7d28.

--- fs/sync.c
+++ fs/sync.c
@@ -202,10 +202,7 @@ static int do_fsync(unsigned int fd, int datasync)
 	struct file *file;
 	int ret = -EBADF;
 	int fput_needed;
-	
-	if (!fsync_enabled)
-		return 0;
-	
+
 	file = fget_light(fd, &fput_needed);
 	if (file) {
 		ret = vfs_fsync(file, datasync);
